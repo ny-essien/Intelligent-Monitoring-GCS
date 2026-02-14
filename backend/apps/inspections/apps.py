6d399341be45193f8ae5a4f1730c4d2dc4cdf15c
Nsikan <nsikan4001@gmail.com>
2026-02-14 20:59:38 +0100

    Completed Environment Setup and installed packages

---
+++ backend/apps/inspections/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class InspectionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.inspections'
+    verbose_name = _("Inspections")
